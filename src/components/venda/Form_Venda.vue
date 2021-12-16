44948793f98f612e5602ed27857b82f34a21a9cb
EriksJunior <EriksJunior13@gmail.com>
2021-12-15 23:33:02 -0300

    commit

--- src/components/venda/Form_Venda.vue
+++ src/components/venda/Form_Venda.vue
@@ -1,10 +1,33 @@
 <template>
-  <div><h1>Forme Venda</h1></div>
+  <div id="cardContainer">
+    <div id="tabContainer">
+      <b-card no-body>
+        <b-tabs card>
+          <b-tab title="Venda" active>
+            <b-card-text>Tab contents 1</b-card-text>
+          </b-tab>
+          <b-tab title="Pesquisa">
+            <b-card-text>Tab contents 2</b-card-text>
+          </b-tab>
+        </b-tabs>
+      </b-card>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {};
 </script>
 
-<style>
+<style scopet>
+#cardContainer {
+  width: 100%;
+  height: 88.6vh;
+  margin-top: 50px;
+}
+
+#tabContainer {
+  width: 80%;
+  margin: 0 auto;
+}
 </style>
