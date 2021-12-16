7cf6ef969c67c3fcba06302377377d8cd1333296
EriksJunior <EriksJunior13@gmail.com>
2021-12-15 22:27:35 -0300

    commit

--- src/components/nav/nav.vue
+++ src/components/nav/nav.vue
@@ -5,14 +5,21 @@
         <b-nav-item to="/">Home</b-nav-item>
 
         <!-- Navbar dropdowns -->
-        <b-nav-item-dropdown text="Cadastros" right>
+        <b-nav-item-dropdown text="Cadastros" left>
           <b-dropdown-item to="/cliente">Cliente</b-dropdown-item>
           <b-dropdown-item to="/produto">Produto</b-dropdown-item>
         </b-nav-item-dropdown>
 
-        <b-nav-item-dropdown text="Controles" right>
+        <b-nav-item-dropdown text="Vendas" left>
+          <b-dropdown-item to="/venda">Pedido de Venda</b-dropdown-item>
+          <b-dropdown-item to="/nfenfce">NFe/NFCe</b-dropdown-item>
+          <b-dropdown-item to="/relatorio">Relatórios</b-dropdown-item>
+        </b-nav-item-dropdown>
+
+        <b-nav-item-dropdown text="Controles" left>
           <b-dropdown-item to="/estoque">Estoque</b-dropdown-item>
           <b-dropdown-item to="/financeiro">Financeiro</b-dropdown-item>
+          <b-dropdown-item to="/caixa">Caixa</b-dropdown-item>
         </b-nav-item-dropdown>
 
         <!-- <b-nav-item-dropdown text="User" right>
