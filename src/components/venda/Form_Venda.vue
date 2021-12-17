3dd92c74224a894097fecce52c919f285ee1839f
EriksJunior <EriksJunior13@gmail.com>
2021-12-16 23:22:01 -0300

    commit

--- src/components/venda/Form_Venda.vue
+++ src/components/venda/Form_Venda.vue
@@ -4,7 +4,23 @@
       <b-card no-body>
         <b-tabs card>
           <b-tab title="Venda" active>
-            <b-card-text>Tab contents 1</b-card-text>
+            <b-card-text>
+              <b-form-row>
+                <div class="form-group col-sm-4">
+                  <b-form-group label="Produto">
+                    <b-form-select hidden> </b-form-select>
+                    <b-form-select
+                      class="col-xl-11"
+                      size="sm"
+                      value-field="id"
+                      text-field="nome"
+                      :options="produto"
+                    >
+                    </b-form-select>
+                  </b-form-group>
+                </div>
+              </b-form-row>
+            </b-card-text>
           </b-tab>
           <b-tab title="Pesquisa">
             <b-card-text>Tab contents 2</b-card-text>
@@ -16,7 +32,17 @@
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      produto: [
+        {
+          nome: "teste",
+        },
+      ],
+    };
+  },
+};
 </script>
 
 <style scopet>
